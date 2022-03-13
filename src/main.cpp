569dfb470ced3a031c2973e676bf6bde9b78aa1b
Yaroslav Yakovlev <ekb-buza@mail.ru>
2022-03-13 18:20:52 +0500

    возможное решение

--- src/main.cpp
+++ src/main.cpp
@@ -3,29 +3,37 @@
 #include <iomanip>
 #include <iostream>
 #include <sstream>
+#include <vector>
 
-double result_A;
-double result_B;
-double result_length;
-double result_scale_Ax;
-double result_scale_Ay;
+struct coordinates_vector {
+  double point_A = 0;
+  double point_B = 0;
+};
 
-void vec_add(double& Ax, double& Ay, double& Bx, double& By) {
-  result_A = Ax + Bx;
-  result_B = Ay + By;
-}
-void vec_subtract(double& Ax, double& Ay, double& Bx, double& By) {
-  result_A = Ax - Bx;
-  result_B = Ay - By;
+std::vector<double> vec_add(std::vector<coordinates_vector>& vec) {
+  std::vector<double> result_A_B;
+  result_A_B.push_back(vec[0].point_A + vec[0].point_B);
+  result_A_B.push_back(vec[1].point_A + vec[1].point_B);
+  return result_A_B;
 }
 
-void vec_scale(double& Ax, double& Ay, double& scale) {
-  result_scale_Ax = std::abs(Ax) * scale;
-  result_scale_Ay = std::abs(Ay) * scale;
+std::vector<double> vec_subtract(std::vector<coordinates_vector>& vec) {
+  std::vector<double> result_A_B;
+  result_A_B.push_back(vec[0].point_A + vec[0].point_B);
+  result_A_B.push_back(vec[1].point_A + vec[1].point_B);
+  return result_A_B;
 }
 
-void vec_length(double& Coordinate_Ax, double& Coordinate_Point_Ay, double& Coordinate_Point_Bx, double& Coordinate_Point_By) {
-  result_length = std::sqrt(std::pow((Coordinate_Point_Bx - Coordinate_Ax), 2) + std::pow((Coordinate_Point_By - Coordinate_Point_Ay), 2));
+std::vector<double> vec_scale(std::vector<coordinates_vector>& vec, double& scale) {
+  std::vector<double> result_scale_A_B;
+  result_scale_A_B.push_back(std::abs(vec[0].point_A) * scale);
+  result_scale_A_B.push_back(std::abs(vec[1].point_B) * scale);
+  return result_scale_A_B;
+}
+
+double vec_length(std::vector<coordinates_vector>& vec) {
+  double result_length = std::sqrt(std::pow((vec[1].point_A - vec[0].point_A), 2) + std::pow((vec[1].point_B - vec[0].point_B), 2));
+  return result_length;
 }
 
 void vec_normalize() {}
@@ -40,51 +48,56 @@ struct math_command {
 
 int main() {
   std::cout << "Realization of mathematical vector" << std::endl;
+  int countVec;
   math_command math_command;
   std::string command;
+  double scale;
   double Ax;
   double Ay;
-  double Bx;
-  double By;
-  double scale;
-
+  std::vector<coordinates_vector> tmp_vector;
   std::cout << "Enter the command over the vector" << std::endl;
   std::cin >> command;
   if (command == math_command.add) {
-    std::cout << "Enter the first vector coordinates" << std::endl;
-    std::cin >> Ax >> Ay;
-    std::cout << "Enter the second vector coordinates" << std::endl;
-    std::cin >> Bx >> By;
-    vec_add(Ax, Ay, Bx, By);
-    std::cout << "Summ vec = " << result_A << " and " << result_B << std::endl;
+    countVec = 2;
+    for (int i = 0; i < countVec; i++) {
+      std::cout << "Enter vector coordinates" << std::endl;
+      std::cin >> Ax >> Ay;
+      tmp_vector.push_back({Ax, Ay});
+    }
+    std::cout << "Summ vec = " << vec_add(tmp_vector)[0] << " and " << vec_add(tmp_vector)[1] << std::endl;
   } else if (command == math_command.subtract) {
-    std::cout << "Enter the first vector coordinates" << std::endl;
-    std::cin >> Ax >> Ay;
-    std::cout << "Enter the second vector coordinates" << std::endl;
-    std::cin >> Bx >> By;
-    vec_subtract(Ax, Ay, Bx, By);
-    std::cout << "Subtract vec = " << result_A << " and " << result_B << std::endl;
+    countVec = 2;
+    for (int i = 0; i < countVec; i++) {
+      std::cout << "Enter vector coordinates" << std::endl;
+      std::cin >> Ax >> Ay;
+      tmp_vector.push_back({Ax, Ay});
+    }
+    std::cout << "Summ vec = " << vec_subtract(tmp_vector)[0] << " and " << vec_subtract(tmp_vector)[1] << std::endl;
   } else if (command == math_command.scale) {
-    std::cout << "Enter the first vector coordinates" << std::endl;
+    countVec = 1;
+    std::cout << "Enter vector coordinates" << std::endl;
     std::cin >> Ax >> Ay;
+    tmp_vector.push_back({Ax, Ay});
     std::cout << "Enter the scale" << std::endl;
     std::cin >> scale;
-    vec_scale(Ax, Ay, scale);
-    std::cout << "Scale vec = " << result_scale_Ax << " and " << result_scale_Ay << std::endl;
+    std::cout << "Scale vec = " << vec_scale(tmp_vector, scale)[0] << " and " << vec_scale(tmp_vector, scale)[1] << std::endl;
   } else if (command == math_command.length) {
-    std::cout << "Enter the first vector coordinates" << std::endl;
-    std::cin >> Ax >> Ay;
-    std::cout << "Enter the second vector coordinates" << std::endl;
-    std::cin >> Bx >> By;
-    vec_length(Ax, Ay, Bx, By);
-    std::cout << "Length vec = " << result_length << std::endl;
+    countVec = 2;
+    for (int i = 0; i < countVec; i++) {
+      std::cout << "Enter vector coordinates" << std::endl;
+      std::cin >> Ax >> Ay;
+      tmp_vector.push_back({Ax, Ay});
+    }
+    std::cout << "Length vec = " << vec_length(tmp_vector) << std::endl;
   } else if (command == math_command.normalize) {
-    std::cout << "Enter the first vector coordinates" << std::endl;
-    std::cin >> Ax >> Ay;
-    std::cout << "Enter the second vector coordinates" << std::endl;
-    std::cin >> Bx >> By;
-    vec_length(Ax, Ay, Bx, By);
-    std::cout << "Normalize vec = " << Ax / result_length << " , " << Ay / result_length << " and " << Bx / result_length << " , " << By / result_length << std::endl;
+    countVec = 2;
+    for (int i = 0; i < countVec; i++) {
+      std::cout << "Enter vector coordinates" << std::endl;
+      std::cin >> Ax >> Ay;
+      tmp_vector.push_back({Ax, Ay});
+    }
+    std::cout << "Normalize vec = " << tmp_vector[0].point_A / vec_length(tmp_vector) << " , " << tmp_vector[0].point_B / vec_length(tmp_vector) << " and "
+              << tmp_vector[1].point_A / vec_length(tmp_vector) << " , " << tmp_vector[1].point_B / vec_length(tmp_vector) << std::endl;
   }
 
   return 0;
