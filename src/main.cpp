0d0cf0493ce89199384d244620ef1d5eb410d14b
Yaroslav Yakovlev <ekb-buza@mail.ru>
2022-03-13 18:36:36 +0500

    оптимизировал функцию заполнения веторов

--- src/main.cpp
+++ src/main.cpp
@@ -19,8 +19,8 @@ std::vector<double> vec_add(std::vector<coordinates_vector>& vec) {
 
 std::vector<double> vec_subtract(std::vector<coordinates_vector>& vec) {
   std::vector<double> result_A_B;
-  result_A_B.push_back(vec[0].point_A + vec[0].point_B);
-  result_A_B.push_back(vec[1].point_A + vec[1].point_B);
+  result_A_B.push_back(vec[0].point_A - vec[0].point_B);
+  result_A_B.push_back(vec[1].point_A - vec[1].point_B);
   return result_A_B;
 }
 
@@ -34,6 +34,15 @@ std::vector<double> vec_scale(std::vector<coordinates_vector>& vec, double& scal
 double vec_length(std::vector<coordinates_vector>& vec) {
   double result_length = std::sqrt(std::pow((vec[1].point_A - vec[0].point_A), 2) + std::pow((vec[1].point_B - vec[0].point_B), 2));
   return result_length;
+}
+
+void filling_vector(std::vector<coordinates_vector>& tmp_vector, int countVec) {
+  double Ax, Ay;
+  for (int i = 0; i < countVec; i++) {
+    std::cout << "Enter vector coordinates" << std::endl;
+    std::cin >> Ax >> Ay;
+    tmp_vector.push_back({Ax, Ay});
+  }
 }
 
 void vec_normalize() {}
@@ -59,20 +68,12 @@ int main() {
   std::cin >> command;
   if (command == math_command.add) {
     countVec = 2;
-    for (int i = 0; i < countVec; i++) {
-      std::cout << "Enter vector coordinates" << std::endl;
-      std::cin >> Ax >> Ay;
-      tmp_vector.push_back({Ax, Ay});
-    }
+    filling_vector(tmp_vector, countVec);
     std::cout << "Summ vec = " << vec_add(tmp_vector)[0] << " and " << vec_add(tmp_vector)[1] << std::endl;
   } else if (command == math_command.subtract) {
     countVec = 2;
-    for (int i = 0; i < countVec; i++) {
-      std::cout << "Enter vector coordinates" << std::endl;
-      std::cin >> Ax >> Ay;
-      tmp_vector.push_back({Ax, Ay});
-    }
-    std::cout << "Summ vec = " << vec_subtract(tmp_vector)[0] << " and " << vec_subtract(tmp_vector)[1] << std::endl;
+    filling_vector(tmp_vector, countVec);
+    std::cout << "Subtract vec = " << vec_subtract(tmp_vector)[0] << " and " << vec_subtract(tmp_vector)[1] << std::endl;
   } else if (command == math_command.scale) {
     countVec = 1;
     std::cout << "Enter vector coordinates" << std::endl;
@@ -83,22 +84,13 @@ int main() {
     std::cout << "Scale vec = " << vec_scale(tmp_vector, scale)[0] << " and " << vec_scale(tmp_vector, scale)[1] << std::endl;
   } else if (command == math_command.length) {
     countVec = 2;
-    for (int i = 0; i < countVec; i++) {
-      std::cout << "Enter vector coordinates" << std::endl;
-      std::cin >> Ax >> Ay;
-      tmp_vector.push_back({Ax, Ay});
-    }
+    filling_vector(tmp_vector, countVec);
     std::cout << "Length vec = " << vec_length(tmp_vector) << std::endl;
   } else if (command == math_command.normalize) {
     countVec = 2;
-    for (int i = 0; i < countVec; i++) {
-      std::cout << "Enter vector coordinates" << std::endl;
-      std::cin >> Ax >> Ay;
-      tmp_vector.push_back({Ax, Ay});
-    }
+    filling_vector(tmp_vector, countVec);
     std::cout << "Normalize vec = " << tmp_vector[0].point_A / vec_length(tmp_vector) << " , " << tmp_vector[0].point_B / vec_length(tmp_vector) << " and "
               << tmp_vector[1].point_A / vec_length(tmp_vector) << " , " << tmp_vector[1].point_B / vec_length(tmp_vector) << std::endl;
   }
-
   return 0;
 }
